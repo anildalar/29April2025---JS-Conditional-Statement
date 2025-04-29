3785f79ae9d5235ac90ed0ecfe097f442adffd27
anildalar <dollorinfotech@gmail.com>
2025-04-29 09:42:17 +0530

    new

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+let a = Number("2"); //String aa
+let b = Number("2"); //String
+
+console.log(typeof a);
+console.log(typeof b);
+
+console.log(a+b); //Concatination Operator
+
+console.log(a+b); //Concatination Operator
+
+
+console.log(typeof String(10));
+
+console.log(("5" - 2 ) )
+console.log( typeof ("5" - 2 ) )
